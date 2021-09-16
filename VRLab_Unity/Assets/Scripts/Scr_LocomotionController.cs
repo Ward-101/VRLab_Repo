8a405ac56931a396b315bd6675c6596bd78afc32
PÄRET Paul <paret.paul@orange.fr>
2021-09-16 10:41:14 +0200

    Add teleportation

---
+++ VRLab_Unity/Assets/Scripts/Scr_LocomotionController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Scr_LocomotionController : MonoBehaviour
+{
+    public XRController teleportRay;
+    public InputHelpers.Button teleportActivationButton;
+    public float activationThreshold = 0.1f;
+
+    private void Update()
+    {
+        if (teleportRay)
+        {
+            teleportRay.gameObject.SetActive(CheckIfActivated(teleportRay));
+        }
+    }
+
+    public bool CheckIfActivated(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, teleportActivationButton, out bool isActivated, activationThreshold);
+        return isActivated;
+    } 
+}
