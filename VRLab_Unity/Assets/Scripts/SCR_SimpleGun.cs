bcdda7f7484561e49565336d42afc8405e2d17ec
Paul PARET <paret.paul@orange.fr>
2021-10-07 19:21:42 +0200

    Add simple feedback to simplegun & try o fix VFX graph

--- VRLab_Unity/Assets/Scripts/SCR_SimpleGun.cs
+++ VRLab_Unity/Assets/Scripts/SCR_SimpleGun.cs
@@ -5,13 +5,33 @@ using UnityEngine.VFX;
 
 public class SCR_SimpleGun : MonoBehaviour
 {
+    public float shotRange;
+    public float shotWidth;
+
     public Transform MuzzleAnchor;
     public GameObject MuzzleFlash;
+    public LayerMask shotableLayer;
+
+    public GameObject bulletDecal;
+    public AudioClip fireSFX;
+
+    private AudioSource gunAudioSource;
+
+    public void Start()
+    {
+        gunAudioSource = GetComponent<AudioSource>();
+    }
 
     public void Fire()
     {
-        //raycast forward 
-        //player sound
+        if (Physics.SphereCast(MuzzleAnchor.position, shotWidth, transform.forward, out RaycastHit hitInfo ,shotableLayer))
+        {
+            Instantiate(bulletDecal, hitInfo.transform.position, Quaternion.Euler(hitInfo.normal));
+
+            //Add a sound that depend on the surface hit
+        }
+
+        gunAudioSource.PlayOneShot(fireSFX);
         MuzzleFlash.GetComponentInChildren<VisualEffect>().Play();
     }
 }
